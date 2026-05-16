c7fe1150c30fc057505b3e5fcc4f82e85ee480d9
tonyfarag2007 <tonywfarag2007@gmail.com>
2026-05-16 15:40:48 -0400

    Added original and compressed file size in byte count, along with compression percentage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+#include <filesystem>
 #include "HuffmanTree.h"
 #include "FrequencyCodes.h"
 #include "BitStreamWriter.h"
@@ -9,6 +9,8 @@
 int main() {
     int frequency[256] = {0};
     std::ifstream file("C:\\Users\\tonyw\\Downloads\\TicTacToeProject\\stats.txt", std::ios_base::binary);
+    std::filesystem::path originalFilePath = "C:\\Users\\tonyw\\Downloads\\TicTacToeProject\\stats.txt";
+    std::filesystem::path compressedFilePath = "C:\\Users\\tonyw\\Downloads\\TicTacToeProject\\stats(compressed).txt";
     char ch;
     std::map<char, std::string> frequencyCodes;
     if (file.is_open()) {
@@ -19,4 +21,8 @@ int main() {
     std::ifstream inputFile("C:\\Users\\tonyw\\Downloads\\TicTacToeProject\\stats.txt", std::ios_base::binary);
     generateCodes(buildHuffmanTree(frequency, 256), "", frequencyCodes);
     writeCompressedFile(&inputFile, frequencyCodes);
+    std::cout << "File Size: "<< std::filesystem::file_size(originalFilePath) << " bytes" << std::endl;
+    std::cout << "Compressed File Size: " << std::filesystem::file_size(compressedFilePath) << " bytes" << std::endl;
+    std::cout << "That is a " << 100.0 - (static_cast<double>(std::filesystem::file_size(compressedFilePath)) /
+        static_cast<double>(std::filesystem::file_size(originalFilePath)) * 100) << "% decrease!" <<std::endl;
 }
